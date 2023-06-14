c43abdf581b35c60ac3f6c1ff8f785459dce8324
WilliamZhu <allwefantasy@gmail.com>
2023-06-14 19:25:20 +0800

    update

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,8 @@
 project=pyjava
-version=0.5.0
+version=0.6.0
 rm -rf ./dist/*
 pip uninstall -y ${project}
 python setup.py sdist bdist_wheel
 cd ./dist/
 pip install ${project}-${version}-py3-none-any.whl && cd -
-# twine upload dist/*
+twine upload dist/*
