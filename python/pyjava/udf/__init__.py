a52594f20c9fd763b35ad47bbc8030653e8d0655
WilliamZhu <allwefantasy@gmail.com>
2023-09-18 16:30:26 +0800

    update

--- python/pyjava/udf/__init__.py
+++ python/pyjava/udf/__init__.py
@@ -241,11 +241,7 @@ class UDFBuilder(object):
         #     print(inst)
         # finally:    
         #     ray.get(udf_master.give_back.remote(index))
-        try :
-            loop = asyncio.get_running_loop()
-        except:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop()
         res = loop.run_until_complete(UDFBuilder.async_apply(ray_context))
         ray_context.build_result([res])
 
