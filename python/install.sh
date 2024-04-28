c56a7709db2b76f7757fbe41f48ff36fa1b409dd
WilliamZhu <allwefantasy@gmail.com>
2024-04-28 18:15:45 +0800

    update

--- python/install.sh
+++ python/install.sh
@@ -1,26 +1,37 @@
-project=pyjava
-version=$(grep -oP '__version__ = "\K[0-9]+\.[0-9]+\.[0-9]+' pyjava/version.py | cut -d '"' -f 1)
+#!/bin/bash
+
+# 项目名称
+project="pyjava"
+
+# 使用Python一行命令提取版本号，减少对grep和cut的依赖
+version=$(python -c "with open('pyjava/version.py') as f: print([line.split('=')[1].strip().strip('\"') for line in f if '__version__' in line][0])")
 echo "Version: $version"
 
-echo "Cleaning dist..."
+# 清理dist目录
+echo "Clean dist"
 rm -rf ./dist/*
 
-echo "Uninstall Pyjava dist..."
+# 卸载当前安装的项目版本
+echo "Uninstall ${project}"
 pip uninstall -y ${project}
 
-echo "Building Pyjava dist..."
+# 构建项目
+echo "Build ${project} ${version}"
 python setup.py sdist bdist_wheel
 cd ./dist/
 
-echo "Installing Pyjava dist..."
+# 安装新构建的项目版本
+echo "Install ${project} ${version}"
 pip install ${project}-${version}-py3-none-any.whl && cd -
 
+# 默认模式设定
+export MODE=${MODE:-"release"}
 
-export MODE=${MODE:-"dev"}
-if [[ ${MODE} == "release" ]];then
+# 发布模式下的操作
+if [[ ${MODE} == "release" ]]; then
  git tag v${version}
- git push gitee v${version}
-#  git push origin v${version}
- echo "Uploading Pyjava dist..."
+ git push origin v${version}  # 请根据实际情况使用正确的远程仓库名
+ echo "Upload ${project} ${version}"
  twine upload dist/*
 fi
+
